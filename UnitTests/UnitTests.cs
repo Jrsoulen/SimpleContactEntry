79f45ef9da4392b5e89c6cfce23a683f4c20b265
Jonathan R Soulen <jacksoulen@gmail.com>
2021-08-12 20:07:21 -0400

    Adding start of unit tests for tdd

--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -1,14 +1,29 @@
+using Newtonsoft.Json;
+using SimpleContactEntry;
 using System;
+using System.IO;
 using Xunit;
 
 namespace UnitTests
 {
     public class UnitTests
     {
+        //[Fact(Skip = "Integration Test")]
         [Fact]
-        public void Test1()
+        public void CreateNewContact()
         {
+            var json = File.ReadAllText(@"./Fixtures/SampleContact.json");
+            Contact contact = JsonConvert.DeserializeObject<Contact>(json);
 
+            LiteDBRepo dBRepo = new LiteDBRepo();
+            dBRepo.CreateContact(contact);
+        }
+
+        [Fact]
+        public void CreateNewAddress()
+        {
+            var json = File.ReadAllText(@"./Fixtures/SampleAddress.json");
+            Address contact = JsonConvert.DeserializeObject<Address>(json);
         }
     }
 }
