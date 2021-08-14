374640a86784dd100765a4f014cba1f7843bd28b
Jonathan R Soulen <jacksoulen@gmail.com>
2021-08-14 16:46:14 -0400

    Stripping template excess and renaming to contact

---
+++ SimpleContactEntry/Controllers/SimpleContactController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace SimpleContactEntry.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SimpleContactController : ControllerBase
+    {
+
+        private readonly ILogger<SimpleContactController> _logger;
+
+        public SimpleContactController(ILogger<SimpleContactController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Contact> Contacts()
+        {
+            var repo = new LiteDBRepo();
+            return repo.GetAllContacts();
+        }
+    }
+}
