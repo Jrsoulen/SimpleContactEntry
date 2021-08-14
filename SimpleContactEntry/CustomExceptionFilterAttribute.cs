e73a351b2009de7770557a999425ad1af0f72c74
Jonathan R Soulen <jacksoulen@gmail.com>
2021-08-14 18:20:34 -0400

    attribute to send prettier exception message

---
+++ SimpleContactEntry/CustomExceptionFilterAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace SimpleContactEntry
+{
+    public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+            context.Result = new ContentResult
+            {
+                Content = $"Error: {exception.Message}",
+                ContentType = "text/plain",
+                StatusCode = (int?)HttpStatusCode.InternalServerError
+            };
+        }
+    }
+}
