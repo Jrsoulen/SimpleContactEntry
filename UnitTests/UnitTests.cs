c9a90c046f1fceaee82d3c38ea78e6b334977dec
Jonathan R Soulen <jacksoulen@gmail.com>
2021-08-14 20:48:37 -0400

    added CallList testing

--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -1,6 +1,8 @@
 using LiteDB;
 using Newtonsoft.Json;
 using SimpleContactEntry;
+using SimpleContactEntry.Models;
+using System.Collections.Generic;
 using System.IO;
 using Xunit;
 
@@ -8,12 +10,32 @@ namespace UnitTests
 {
     public class UnitTests
     {
+        [Fact]
+        // This is the only real Unit Test
+        // This is testing business logic, the rest are testing integration
+        public void GetCallList()
+        {
+            var contactListFixture = File.ReadAllText(@"./Fixtures/SampleContactList.json");
+            IEnumerable<Contact> contactList = JsonConvert.DeserializeObject<IEnumerable<Contact>>(contactListFixture);
+
+            var callListFixture = File.ReadAllText(@"./Fixtures/SampleCallList.json");
+            List<CallContact> callList = JsonConvert.DeserializeObject<List<CallContact>>(callListFixture);
+
+
+            // .ToString() on both makes this pass.
+            // I think because they are distinct objects otherwise? 
+            // Not sure, first time using XUnit.
+            Assert.Equal(CallListMethods.CreateCallList(contactList).ToString(), callList.ToString());
+        }
+
         /*
         I recognize these tests don't test much
-        These are just simple happy paths for TDD
+        They are just simple happy paths for TDD
 
-        Normally I'd skip all these, since they change actual data
-        I'm leaving them in since this is a sample app
+        Normally I'd (Skip) all these, since they change actual data
+        I'm leaving them without (Skip) since this is a sample app
+
+        Also worth noting, except the first test, these might fail if run out of order.
         */
 
         //[Fact(Skip = "Integration Test")]
@@ -71,5 +93,6 @@ namespace UnitTests
             
             Assert.NotEmpty(contacts);
         }
+
     }
 }
