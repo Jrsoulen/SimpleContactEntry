c6d8d16678ebc5b01218e9a7ec2ed942f404cb2a
Jonathan R Soulen <jacksoulen@gmail.com>
2021-08-14 20:48:22 -0400

    Added ContactList to CallList logic

---
+++ SimpleContactEntry/CallListMethods.cs
@@ -0,0 +1,28 @@
+﻿using SimpleContactEntry.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleContactEntry
+{
+    public static class CallListMethods
+    {
+        public static List<CallContact> CreateCallList(IEnumerable<Contact> contactList)
+        {
+            List<CallContact> callContacts = new List<CallContact>();
+
+            foreach (var contact in contactList)
+            {
+                if (contact.Phone.Any(p => p.Type.ToLower() == "home"))
+                {
+                    var callContact = new CallContact()
+                    {
+                        Name = contact.Name,
+                        Phone = contact.Phone.Where(p => p.Type.ToLower() == "home").First().Number
+                    };
+                    callContacts.Add(callContact);
+                }
+            }
+            return callContacts.OrderBy(c => c.Name.Last).ThenBy(c => c.Name.First).ToList();
+        }
+    }
+}
---
+++ SimpleContactEntry/Models/CallContact.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace SimpleContactEntry.Models
+{
+    public class CallContact
+    {
+        [JsonProperty("name")]
+        public Name Name { get; set; }
+
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+    }
+}
